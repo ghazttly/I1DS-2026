f41abc1c8b6fc0031d3f83ded9b26440df0afbd0
ghazttly <yasminviaro7@gmail.com>
2026-03-25 11:17:58 -0300

    5/50 exercícios

--- atividades/50 exercícios😨😨😨.js
+++ atividades/50 exercícios😨😨😨.js
@@ -1,1 +1,42 @@
 //parte 1.
+
+var nome = "Sérgio";
+var idade = 54;
+var cidade = "Venha-ver";
+
+console.log("---------------------------------------------");
+
+var v1 = 17;
+var v2 = 22;
+
+console.log("Soma : v1 + v2 =", v1 + v2);
+console.log("subtração : v1 - v2 =", v1 - v2);
+console.log("multiplicação : v1 * v2 =", v1 * v2);
+console.log("divisão : v1 / v2 =", v1 / v2);
+console.log("resto divisão : v1 % v2 =", v1 % v2);
+
+console.log("---------------------------------------------");
+
+var lado1 = 9;
+var lado2 = 5;
+
+console.log("area de um retangulo 9 x 5 = ", lado1 * lado2);
+
+console.log("---------------------------------------------");
+
+var lado = 7;
+var compr = 12;
+var altura = 5;
+
+console.log("volume de um paralelepipedo 7 x 12 x 5 = ", lado * compr * altura);
+
+console.log("---------------------------------------------");
+
+var product = 33
+var desconto = 10/100
+var valorFinal = product-(product * desconto)
+
+console.log("desconto: ", valorFinal)
+
+console.log("---------------------------------------------");
+
