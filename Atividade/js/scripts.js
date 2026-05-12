c694d514bf66f0b5cad6f049f1227c3080ec695e
ghazttly <yasminviaro7@gmail.com>
2026-05-12 13:34:56 -0300

    adiçao da pagina produtos codigo da pagina produtos

--- Atividade/js/scripts.js
+++ Atividade/js/scripts.js
@@ -48,3 +48,67 @@ const cadastrar = (event) => {
     )};
 
 }};
+
+let produtos = [];
+let totalGeral = 0;
+
+const adicionarProduto = () => {
+  let nome = document.getElementById("produto").value;
+  let qtd = parseInt(document.getElementById("quantidade").value);
+  let valor = parseFloat(document.getElementById("valor").value);
+
+  if (!nome || !qtd || !valor) {
+    alert("Preencha todos os campos");
+    return;
+  }
+  let total = qtd * valor;
+  produtos.push({
+    nome,
+    qtd,
+    valor,
+    total,
+  });
+  atualizarTabela();
+  document.getElementById("produto").value = "";
+  document.getElementById("quantidade").value = "";
+  document.getElementById("valor").value = "";
+};
+
+const atualizarTabela = () => {
+  let tbody = document.querySelector("#tabelaProdutos tbody");
+  tbody.innerHTML = "";
+  produtos.forEach((item) => {
+    tbody.innerHTML += `
+      <tr>
+        <td>${item.nome}</td>
+        <td>${item.qtd}</td>
+        <td>${item.valor.toFixed(2)}</td>
+        <td>${item.total.toFixed(2)}</td>
+      </tr>
+    `;
+  });
+};
+const finalizarCompra = () => {
+  totalGeral = 0;
+  produtos.forEach((item) => {
+    totalGeral += item.total;
+  });
+  document.getElementById("total").innerText = totalGeral.toFixed(2);
+  document.getElementById("valorLiquido").innerText = totalGeral.toFixed(2);
+};
+
+const aplicarDesconto = () => {
+  let descontoValor =
+    parseFloat(document.getElementById("descontoValor").value) || 0;
+
+    let descontoPercentual =
+    parseFloat(document.getElementById("descontoPercentual").value) || 0;
+  let valorFinal = totalGeral;
+  if (descontoValor > 0) {
+    valorFinal -= descontoValor;
+  }
+  if (descontoPercentual > 0) {
+    valorFinal -= totalGeral * (descontoPercentual / 100);
+  }
+  document.getElementById("valorLiquido").innerText = valorFinal.toFixed(2);
+};
